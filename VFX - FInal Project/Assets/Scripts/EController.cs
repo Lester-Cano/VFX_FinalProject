dc2a50a6ab6486f70977618d232dc5d07ae63796
Nestor <nestcano2971@gmail.com>
2022-05-12 20:48:14 -0500

    E ability

---
+++ VFX - FInal Project/Assets/Scripts/EController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EController : MonoBehaviour
+{
+    [SerializeField] ParticleSystem[] particleSystem;
+    [SerializeField] ParticleSystem particleSystemHip;
+    [SerializeField] GameObject shield;
+
+
+}
