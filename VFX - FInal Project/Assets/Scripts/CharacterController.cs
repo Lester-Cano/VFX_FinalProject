0d01e5642484e6778dfe9f316e59f2928c395560
Julian Rosero <julian.rosero@outlook.es>
2022-05-15 18:44:51 -0500

    Q - Animations

--- VFX - FInal Project/Assets/Scripts/CharacterController.cs
+++ VFX - FInal Project/Assets/Scripts/CharacterController.cs
@@ -9,11 +9,7 @@ public class CharacterController : MonoBehaviour
     private PlayerController _playerController;
     [SerializeField] private GameObject character;
     private Animator _animator;
-    
-    //Q skill
 
-    private bool _equipped;
-    
     private void Awake()
     {
         _playerController = new PlayerController();
@@ -39,17 +35,6 @@ public class CharacterController : MonoBehaviour
         if (_playerController.Skills.QSkill.triggered)
         {
             Debug.Log("Q Skill");
-            
-            if (_equipped)
-            {
-                _animator.SetTrigger("Q Shoot");
-                _equipped = false;
-            }
-            else
-            {
-                _animator.SetTrigger("Q Equip");
-                _equipped = true;
-            }
         }
         
         if (_playerController.Skills.ESkill.triggered)
